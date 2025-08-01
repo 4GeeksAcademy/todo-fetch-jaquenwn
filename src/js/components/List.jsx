d0a7432cb30fe8b526950be0b1f8571c4c089f78
Jaqueline Téllez <78996295+jaquenwn@users.noreply.github.com>
2025-08-01 20:00:32 +0000

    input

--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -1,50 +1,110 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const List = () => {
 
-  const [task, setTask] = useState('');
-  const [taskList, setTaskList] = useState([
-    "walk the dog",
-    "wash the dishes",
-    "do the laundry"
-  ]);
+  const [task, setTask] = useState({
+    "label": "",
+    "is_done": false
+  });
+  const [taskList, setTaskList] = useState([]);
 
-  console.log(task)
+  const getList = async () => {
+    try {
+      const res = await fetch("https://playground.4geeks.com/todo/users/jaque")
+      console.log(res)
+      if (res.status === 404) {
+        crearUsuario()
+        return
+      }
+      const data = await res.json()
+      console.log(data.todos)
+      setTaskList(data.todos)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const crearUsuario = async () => {
+    try {
+      const newUser = await fetch("https://playground.4geeks.com/todo/users/jaque", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      console.log(newUser)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const removeTask = (index) => {
+    try {
+      const deleteTask = fetch(
+        "https://playground.4geeks.com/todo/todos/jaquenwn",
+        {
+          method: 'DELETE',
+          body: JSON.stringify(taskList[index]),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+    } catch (error) {
+      console.log(error)
+    }
     let aux1 = [...taskList];
     aux1.splice(index, 1);
     setTaskList(aux1)
   }
-  
-   const newTask =(e) => {
-    e.preventDefault(); 
-    if (task.trim() === "") return;
-    
-    setTaskList([task, ...taskList]); 
-    setTask(''); 
-  };
-   const items= taskList.length;
-   const noTasks = "There are no tasks, add one:)"
-   const yesTasks= items + " items left"
 
+  const newTask = async (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      console.log(task.label)
+      try {
+        const postTask = await fetch("https://playground.4geeks.com/todo/todos/jaque", {
+          method: 'POST',
+          body: JSON.stringify({
+            "label": task.label,
+            "is_done": false
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        })
+        console.log(postTask)
+      } catch (error) {
+        console.log(error)
+      }
+
+
+      setTask({
+        "label": "",
+        "is_done": false
+      });
+    }
+  }
+  const items = taskList.length;
+  const noTasks = "There are no tasks, add one:)"
+  const yesTasks = items + " items left"
+
+  useEffect(() => {
+    getList()
+  }, [])
   return (
     <div className="card my-3 col-10" >
-      <form onSubmit={newTask}>
-        <input type="text" placeholder= "What needs to be done?" className="border p-2 col-12" onChange={e => setTask(e.target.value)} value={task} />
+      <form >
+        <input onKeyDown={(e) => newTask(e)} type="text" placeholder="What needs to be done?" className="border p-2 col-12" onChange={(e) => setTask(e.target.value)} value={task.label} />
       </form>
       <div className=" w-100 " >
         {
           taskList.map((task, index) =>
             <div className="d-flex border-top" key={index} >
-              <div className="col-11 text-start p-2">{task}</div>
-              <button  className="d-flex border-0  bg-white col-1 p-2 " onClick={() => removeTask(index)}>x</button>
+              <div className="col-11 text-start p-2">{task.label}</div>
+              <button className="d-flex border-0  bg-white col-1 p-2 " onClick={() => removeTask(index)}> ✔</button>
             </div>
           )
         }
       </div>
       <div className="card-footer text-start text-muted">
-        <small>{items === 0? noTasks: yesTasks}</small>
+        <small>{items === 0 ? noTasks : yesTasks}</small>
       </div>
     </div>
   )
